0c681e4cd8a533c20c218ff507a0c5ba80c874cc
Ovais Butt <ovaisbutt786@gmail.com>
2018-10-04 16:39:18 +0500

    NOTE DB created

--- app/src/main/java/com/example/hassaan/leadcrm/Repo/NoteRepo.java
+++ app/src/main/java/com/example/hassaan/leadcrm/Repo/NoteRepo.java
@@ -1,4 +1,22 @@
 package com.example.hassaan.leadcrm.Repo;
 
+import com.example.hassaan.leadcrm.TableClasses.Event;
+import com.example.hassaan.leadcrm.TableClasses.Note;
+
 public class NoteRepo {
+
+    public Note note;
+    public NoteRepo()
+    {
+        note = new Note();
+    }
+
+    public static String createTable (){
+        return "CREATE TABLE" + Note.TABLE_NAME +
+                " (" + Note.KEY_ID + "INTEGER PRIMARY KEY AUTOINCREMENT , " +
+                Note.KEY_TITLENAME + "VARCHAR, " +
+                Note.KEY_DETAILS + "VARCHAR, " +
+                Note.KEY_LEADID + "INTEGER, " +
+                Note.CREATED_DATE + "DATETIME )" ;
+    }
 }
--- app/src/main/java/com/example/hassaan/leadcrm/TableClasses/Note.java
+++ app/src/main/java/com/example/hassaan/leadcrm/TableClasses/Note.java
@@ -1,4 +1,68 @@
 package com.example.hassaan.leadcrm.TableClasses;
 
 public class Note {
+
+    public static final String TABLE_NAME = "tbl_Note";
+    public static final String KEY_ID = "ID";
+    public static final String KEY_TITLENAME = "TitleName";
+    public static final String KEY_DETAILS = "Details";
+    public static final String KEY_LEADID = "LeadID";
+    public static final String CREATED_DATE = "CreatedDate";
+
+    public String ID;
+    public String TitleName;
+    public String Details;
+    public String LeadID;
+    public String CreatedDate;
+
+    public Note(String ID, String titleName, String details, String leadID, String createdDate) {
+        this.ID = ID;
+        this.TitleName = titleName;
+        this.Details = details;
+        this.LeadID = leadID;
+        this.CreatedDate = createdDate;
+    }
+
+    public Note()
+    {}
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTitleName() {
+        return TitleName;
+    }
+
+    public void setTitleName(String titleName) {
+        TitleName = titleName;
+    }
+
+    public String getDetails() {
+        return Details;
+    }
+
+    public void setDetails(String details) {
+        Details = details;
+    }
+
+    public String getLeadID() {
+        return LeadID;
+    }
+
+    public void setLeadID(String leadID) {
+        LeadID = leadID;
+    }
+
+    public String getCreatedDate() {
+        return CreatedDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        CreatedDate = createdDate;
+    }
 }
