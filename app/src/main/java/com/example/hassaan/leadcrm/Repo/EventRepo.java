d57fd2f1ef403cbf4a1d8678e6585a63e59cf13f
Ovais Butt <ovaisbutt786@gmail.com>
2018-10-04 16:21:43 +0500

    Event DB created

--- app/src/main/java/com/example/hassaan/leadcrm/Repo/EventRepo.java
+++ app/src/main/java/com/example/hassaan/leadcrm/Repo/EventRepo.java
@@ -1,4 +1,24 @@
 package com.example.hassaan.leadcrm.Repo;
 
+import com.example.hassaan.leadcrm.TableClasses.Event;
+
 public class EventRepo {
+
+    private Event event;
+    public EventRepo()
+    {
+        event = new Event();
+    }
+
+    public static String createTable(){
+        return "CREATE TABLE" + Event.TABLE_NAME +
+                " (" + Event.KEY_ID + "INTEGER PRIMARY KEY AUTOINCREMENT , " +
+                Event.KEY_LOCATIONS + "VARCHAR, " +
+                Event.KEY_STARTDATE + "DATETIME, " +
+                Event.KEY_ENDDATE + "DATETIME, " +
+                Event.KEY_HOSTNAME + "VARCHAR, " +
+                Event.KEY_EVENTNAME + "VARCHAR, " +
+                Event.KEY_STARTTIME + "TIME, " +
+                Event.KEY_ENDTIME + "TIME )" ;
+    }
 }
