e97196d0b8c99720f20e1faeabd6e95dfb9b3aaf
Zohaib Javed <Zohaibjaved2002@gmail.com>
2018-10-04 17:26:32 +0500

    After status

--- app/src/main/java/com/example/hassaan/leadcrm/Repo/StatusRepo.java
+++ app/src/main/java/com/example/hassaan/leadcrm/Repo/StatusRepo.java
@@ -1,4 +1,16 @@
 package com.example.hassaan.leadcrm.Repo;
 
+import com.example.hassaan.leadcrm.TableClasses.Status;
+
 public class StatusRepo {
+    private Status status;
+    public StatusRepo() {
+        status = new Status();
+    }
+
+    public static String createTable() {
+        return "CREATE TABLE " + Status.TABLE_NAME +
+                " (" + Status.KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Status.KEY_Name + " VARCHAR)";
+    }
 }
