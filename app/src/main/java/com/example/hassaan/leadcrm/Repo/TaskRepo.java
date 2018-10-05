8a125d70b72e42eda6a52943af0b12a8cd29dcb7
Ovais Butt <ovaisbutt786@gmail.com>
2018-10-05 09:44:19 +0500

    TASK Foreign Key added

--- app/src/main/java/com/example/hassaan/leadcrm/Repo/TaskRepo.java
+++ app/src/main/java/com/example/hassaan/leadcrm/Repo/TaskRepo.java
@@ -1,5 +1,7 @@
 package com.example.hassaan.leadcrm.Repo;
 
+import com.example.hassaan.leadcrm.TableClasses.Priority;
+import com.example.hassaan.leadcrm.TableClasses.Status;
 import com.example.hassaan.leadcrm.TableClasses.Task;
 
 public class TaskRepo {
@@ -12,9 +14,10 @@ public class TaskRepo {
                 " (" + Task.KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 Task.KEY_SUBJECT + " VARCHAR, " +
                 Task.KEY_DUEDATE + " DATETIME, " +
-                Task.KEY_PRIORITYID + " INTEGER , " +
-                Task.KEY_OWNERNAME + " VARCHAR , " +
+                Task.KEY_PRIORITYID + " INTEGER, " +
+                "FOREIGN KEY (" + Task.KEY_PRIORITYID +") REFERENCES " + Priority.TABLE_NAME + "(" + Priority.KEY_ID + ")" +
+                Task.KEY_OWNERNAME + " VARCHAR, " +
                 Task.KEY_STATUSID + " INTEGER " +
-                ")";
+                " FOREIGN KEY ("+ Task.KEY_STATUSID +") REFERENCES "+ Status.TABLE_NAME +"("+ Status.KEY_ID +"));";
     }
 }
