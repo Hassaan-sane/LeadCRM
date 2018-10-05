1a68717eb156da00a4b6fd0dc462f5e7ebd27d1e
Zohaib Javed <Zohaibjaved2002@gmail.com>
2018-10-05 13:20:52 +0500

    Navigation drawer implemented successfully

---
+++ app/src/main/java/com/example/hassaan/leadcrm/Main2Activity.java
@@ -0,0 +1,172 @@
+package com.example.hassaan.leadcrm;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.hassaan.leadcrm.Data.DBHelper;
+
+public class Main2Activity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    DBHelper dbHelper;
+
+    EditText edit1, edit2, edit3;
+
+    Button button, button2 ;
+
+    TextView view1, view2, view3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //self code
+        dbHelper = new DBHelper(this);
+
+        edit1 = findViewById(R.id.et1);
+        edit2 = findViewById(R.id.et2);
+        edit3 = findViewById(R.id.et3);
+
+        view1 = findViewById(R.id.tv1);
+        view2 = findViewById(R.id.tv2);
+        view3 = findViewById(R.id.tv3);
+
+        button = findViewById(R.id.btt1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateData();
+            }
+        });
+
+        button2 = findViewById(R.id.btt2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = dbHelper.getAllData();
+                if(res.getCount() == 0)
+                {
+                    Toast.makeText(Main2Activity.this, "No Data Found", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                StringBuffer stringBuffer = new StringBuffer();
+                while(res.moveToNext()){
+                    stringBuffer.append("Title :" + res.getString(2) + "\n");
+                    stringBuffer.append("Phone :" + res.getString(3) + "\n");
+                    stringBuffer.append("Mobile :" + res.getString(4) + "\n");
+                }
+
+                view1.setText(stringBuffer.toString());
+            }
+        });
+            //self code ends here
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main2, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private void updateData() {
+        String a = edit1.getText().toString();
+        String b = edit2.getText().toString();
+        String c = edit3.getText().toString();
+
+        boolean isInserted = dbHelper.insertData(a,b,c);
+        if (isInserted)
+        {
+            Toast.makeText(Main2Activity.this,"Data Inserted",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(Main2Activity.this,"Data NOT Inserted",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
